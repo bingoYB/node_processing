9944b53673f7b88e4075b98008d53e68f86e8ec7
长空 <changkong@gaoding.com>
2022-12-08 21:55:45 +0800

    feat: add reource

---
+++ resource/clash.mjs
@@ -0,0 +1,9 @@
+const clashList = [
+    "https://vercel.daycat.space/Clash.yaml",
+    "https://openit.daycat.space/clash",
+    "https://sub.xeton.dev/sub?target=clash&new_name=true&url=https%3A%2F%2Ffreefq.neocities.org%2Ffree.txt&insert=false&config=https%3A%2F%2Fraw.githubusercontent.com%2FACL4SSR%2FACL4SSR%2Fmaster%2FClash%2Fconfig%2FACL4SSR_Online_Mini.ini",
+    "https://ghproxy.com/https://raw.githubusercontent.com/kxswa/k/k/clash",
+    "https://cdn.jsdelivr.net/gh/oslook/clash-freenode@main/clash.yaml",
+    "https://proxypool.toshiki.top/clash/proxies",
+    "https://api.subcloud.xyz/sub?target=clash&url=https%3A%2F%2Fpastebin.com%2Fraw%2FGxR5pELe&insert=false&config=https%3A%2F%2Fraw.githubusercontent.com%2FACL4SSR%2FACL4SSR%2Fmaster%2FClash%2Fconfig%2FACL4SSR_Online.ini&emoji=true&list=false&tfo=false&scv=true&fdn=true&sort=true&udp=true&new_name=true"
+]
---
+++ resource/v2ray.mjs
@@ -0,0 +1,5 @@
+const v2rayList= [
+    "https://feiniaoyun.xyz/api/v1/client/subscribe?token=b3f9ae13396bec7ee8d720bb3ed6e85b",
+    "https://raw.gitslow.tk/yu-steven/openit/main/long",
+    "https://openit.ml/long"
+]
