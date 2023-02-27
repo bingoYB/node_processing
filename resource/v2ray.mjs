e7b26cf3a564929e088afe6ade868f84ff1991bc
Notus Yung <122308837+ostionig@users.noreply.github.com>
2023-02-27 12:27:24 +0000

    update: add task

--- resource/v2ray.mjs
+++ resource/v2ray.mjs
@@ -1,5 +1,4 @@
 const v2rayList= [
-    "https://feiniaoyun.xyz/api/v1/client/subscribe?token=b3f9ae13396bec7ee8d720bb3ed6e85b",
-    "https://raw.gitslow.tk/yu-steven/openit/main/long",
-    "https://openit.ml/long"
+    "https://raw.githubusercontent.com/freefq/free/master/v2",
+    "https://raw.githubusercontent.com/kxswa/k/k/base64"
 ]
