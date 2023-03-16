eb9046de0a828f569a5e8b7f3fc1c24ca7a48d2e
Notus Yung <122308837+ostionig@users.noreply.github.com>
2023-03-16 07:41:50 +0000

    fix： filter not support type

--- index.mjs
+++ index.mjs
@@ -1,6 +1,10 @@
-import { clashList } from "./resource/clash.mjs"
+import { clashList } from "./resource/clash.mjs";
 import { v2rayList } from "./resource/v2ray.mjs";
-import { generateClashConf, getClashNodesByContent, mergeClashNodes } from "./src/clash.mjs"
+import {
+  generateClashConf,
+  getClashNodesByContent,
+  mergeClashNodes,
+} from "./src/clash.mjs";
 import { logger } from "./src/log.mjs";
 import { getNodeFreeOrg } from "./src/nodefreeOrg.mjs";
 import { generateFile } from "./src/output.mjs";
@@ -8,29 +12,38 @@ import { testSpeed } from "./src/speedTest.mjs";
 import { batchV2rayToClashNodes } from "./src/v2ray.mjs";
 
 async function task1() {
-   const nodeList = await mergeClashNodes(clashList);
+  const nodeList = await mergeClashNodes(clashList);
 
-   const freeNodeContent = await getNodeFreeOrg(0, "yaml");
+  const freeNodeContent = await getNodeFreeOrg(0, "yaml");
 
-   const freeNode = await getClashNodesByContent(freeNodeContent.replaceAll("!<str>",""));
+  const freeNode = await getClashNodesByContent(
+    freeNodeContent.replaceAll("!<str>", "")
+  );
 
-   const v2rayToClashNodes = await batchV2rayToClashNodes(v2rayList);
+  const v2rayToClashNodes = await batchV2rayToClashNodes(v2rayList);
 
-   let allNodes = [...nodeList, ...freeNode, ...v2rayToClashNodes];
-   // for (let i = 0; i < allNodes.length; i++) {
-   //    const proxy = allNodes[i];
-   //    console.log(proxy);
-   //    await testSpeed(proxy).then(rs=>{
-   //       console.log(rs);
-   //    })
-   // }
+  let allNodes = [...nodeList, ...freeNode, ...v2rayToClashNodes];
+  // for (let i = 0; i < allNodes.length; i++) {
+  //    const proxy = allNodes[i];
+  //    console.log(proxy);
+  //    await testSpeed(proxy).then(rs=>{
+  //       console.log(rs);
+  //    })
+  // }
 
-   const configContent = generateClashConf(allNodes.filter(node=>node).filter(node => !node.name.includes("中国")).filter(node => !String(node.password).includes("<")));
+  const configContent = generateClashConf(
+    allNodes
+      .filter((node) => node)
+      .filter((node) => !node.name.includes("中国"))
+      .filter((node) => !String(node.password).includes("<"))
+      // 过滤不支持的vless协议
+      .filter((node)=> node.type !== "vless")
+  );
 
-   const comments = `# 更新时间 ${new Date().toISOString()}
-`
+  const comments = `# 更新时间 ${new Date().toISOString()}
+`;
 
-   generateFile("clashMerge", comments + configContent);
+  generateFile("clashMerge", comments + configContent);
 }
 
 task1();
