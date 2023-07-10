52d20cd51ddfe86099aab4217f9dc91b8a9b80a4
Notus Yung <122308837+ostionig@users.noreply.github.com>
2023-07-10 14:29:10 +0800

    Update v2ray.mjs

--- resource/v2ray.mjs
+++ resource/v2ray.mjs
@@ -1,4 +1,5 @@
 export const v2rayList= [
     "https://raw.githubusercontent.com/freefq/free/master/v2",
-    "https://raw.githubusercontent.com/kxswa/k/k/base64"
+    "https://raw.githubusercontent.com/kxswa/k/k/base64",
+    "https://raw.githubusercontent.com/Pawdroid/Free-servers/main/sub"
 ]
